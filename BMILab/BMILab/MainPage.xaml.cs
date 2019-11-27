827fa1409a6586523dd125789c945e299ecbb062
DEAN MC GOWAN <G00369547@gmit.ie>
2019-11-27 18:30:11 +0000

    Add project files.

---
+++ BMILab/BMILab/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BMILab
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Clicked(object sender, EventArgs e)
+        {
+            float Height = (float)Convert.ToDecimal(HeightEntry.Text);
+            float Weight = (float)Convert.ToDecimal(WeightEntry.Text);
+            float bmi = Weight / (Height * Height);
+            lblAnswer.Text = "Your BMI is : " + bmi.ToString();
+        }
+    }
+}
